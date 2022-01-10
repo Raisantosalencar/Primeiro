3e6f8a574b02f9561809e5253d6fb4c176adddb4
alencar <raianesantosalencar@gmail.com>
2022-01-10 14:28:36 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Primeiro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var a = "44((599";
+			var b = 55242.15;
+
+			var c = a + b;
+
+			Console.WriteLine(a + b);
+
+
+
+		}
+	}
+}
